752d8b897880e748d8b0c0172b5a0ca7924bc45d
chykB <malikchika86@gmail.com>
2023-05-25 00:41:34 +0000

    updated

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -52,9 +52,9 @@ class User():
         """
         if pwd is None or type(pwd) is not str:
             return False
-        if self.__password is None:
+        if self.password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.password
 
 
 if __name__ == '__main__':
@@ -84,13 +84,11 @@ if __name__ == '__main__':
     if user_2.password is not None:
         print("User.password should be None if setter to an integer")
 
-    if not user_1.is_valid_password(u_pwd):
-        print("is_valid_password should return True if it's the right \
-password")
+    if not user_1.is_valid_password(u_pwd);
+        print("is_valid_password should return True if it's the right password")
 
     if user_1.is_valid_password("Fakepwd"):
-        print("is_valid_password should return False if it's not the right \
-password")
+        print("is_valid_password should return False if it's not the right password")
 
     if user_1.is_valid_password(None):
         print("is_valid_password should return False if compare with None")
@@ -99,5 +97,4 @@ password")
         print("is_valid_password should return False if compare with integer")
 
     if user_2.is_valid_password("No pwd"):
-        print("is_valid_password should return False if no password set \
-before")
+        print("is_valid_password should return False if no password set before")
